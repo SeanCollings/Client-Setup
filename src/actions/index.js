9b41835129d8b837d24b9655a33bc8bec68538b4
collingss <nightharrier@gmail.com>
2018-01-16 15:09:16 +0200

    first commit - client validation

---
+++ src/actions/index.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER, AUTH_ERROR, UNAUTH_USER } from './types';
+
+const ROOT_URL = 'http://localhost:3090';
+
+export function signinUser({ email, password }) {
+  return function(dispatch) {
+    // Submit email/password to the server
+    axios
+      .post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // If request is good...
+        // - update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
+        // - save the JWT token
+        localStorage.setItem('token', response.data.token);
+        // - redirect to the route '/feature' using programmatic navigation
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // If request is bad...
+        // - show error to the user
+        dispatch(authError('Bad Login Info'));
+      });
+  };
+}
+
+export function signupUser({ email, password }) {
+  return function(dispatch) {
+    axios
+      .post(`${ROOT_URL}/signup`, { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+        browserHistory.push('/feature');
+      })
+      .catch(error => {
+        dispatch(authError(error.response.data.error));
+      });
+  };
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  };
+}
+
+export function signoutUser() {
+  localStorage.removeItem('token');
+
+  return { type: UNAUTH_USER };
+}
